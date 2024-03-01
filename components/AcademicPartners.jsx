fd97f94961e3dd402c7a950d2167e64dffc55c25
sai-ram-kaushik <sairamkaushik1@gmail.com>
2024-03-01 23:53:25 +0530

    updated with the academic side

--- components/AcademicPartners.jsx
+++ components/AcademicPartners.jsx
@@ -1,26 +1,18 @@
-import { menuList } from "@/constants";
-import Link from "next/link";
 import React from "react";
-
-const SideBar = () => {
+import data from "@/data/academic-partners.json";
+const AcademicPartners = () => {
+  const {
+    academicPartners: { title },
+  } = data;
   return (
-    <div className=" px-5 lg:px-10 py-10 border-l lg:sticky lg:top-0">
-      <div className="container mx-auto w-full">
-        <div className="flex flex-col items-start gap-5">
-          {menuList.map((list, index) => {
-            return (
-              <ul className="min-w-[300px] bg-secondary px-3 py-2 rounded-lg text-background font-semibold" key={index}>
-                <Link href={list.path} className="flex items-center gap-3">
-                  <li className="text-background">{list.icon}</li>
-                  <li>{list.label}</li>
-                </Link>
-              </ul>
-            );
-          })}
+    <div className="px-5 lg:px-10 py-10 min-w-[300px] lg:sticky lg:top-0 border-l">
+      <div className="container mx-auto">
+        <div className="flex flex-col items-center justify-center">
+          <h3 className="text-center">{title}</h3>
         </div>
       </div>
     </div>
   );
 };
 
-export default SideBar;
+export default AcademicPartners;
