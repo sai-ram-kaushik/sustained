bd9f89a6e51457fdc3aafdc17899b36f99298533
Tabish Naqvi <tabish.naqvi2003@gmail.com>
2024-09-28 15:50:12 +0530

    updated call for paper dates

--- app/call-for-papers/page.jsx
+++ app/call-for-papers/page.jsx
@@ -1,86 +1,144 @@
-'use client';
+"use client";
 
 import data from "../../data/call-for-papers.json";
 import { guidlines } from "../../constants/index";
 import Link from "next/link";
 
-
 const CallForPapers = () => {
-  const {
-    callForPapers: { title, subTitle, content },
-  } = data;
+    const {
+        callForPapers: { title, subTitle, content },
+    } = data;
 
+    return (
+        <div className="px-5 lg:px-10 py-10">
+            <div className="container mx-auto">
+                <div className="mt-10 flex flex-col gap-3">
+                    <h3>Submit Full Length Paper</h3>
+                    <p>
+                        Authors are requested to submit full-length papers
+                        online using the following web link:
+                    </p>
 
-  return (
-    <div className="px-5 lg:px-10 py-10">
-      <div className="container mx-auto">
+                    <a
+                        href="https://cmt3.research.microsoft.com/SUSTAINED2024/"
+                        className="text-secondary underline block break-all max-w-full"
+                    >
+                        https://cmt3.research.microsoft.com/SUSTAINED2024/{" "}
+                    </a>
+                </div>
 
-        <div className="mt-10 flex flex-col gap-3">
-          <h3>Submit Full Length Paper</h3>
-          <p>Authors are requested to submit full-length papers online using the following web link:
-          </p>
+                <div className=" flex flex-col gap-5 mt-10">
+                    <h3>Important Dates</h3>
+                    <table>
+                        <tbody>
+                            <tr>
+                                <td>
+                                    &nbsp;Deadline for Full Paper Submission
+                                </td>
+                                <td>
+                                    <span className="line-through decoration-red-500 text-black">
+                                        <strong>
+                                            &nbsp;30<sup>th</sup>&nbsp;September
+                                            2024
+                                        </strong>
+                                        &nbsp;
+                                    </span>
+                                    <strong className="text-red-500">
+                                        &nbsp;31<sup>st</sup>&nbsp;October 2024
+                                    </strong>
+                                    &nbsp;
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    &nbsp;Notification of Paper
+                                    Acceptance/Rejection
+                                </td>
+                                <td>
+                                    <span className="line-through decoration-red-500 text-black">
+                                        &nbsp;
+                                        <strong>
+                                            10<sup>th</sup>&nbsp;October 2024
+                                        </strong>
+                                    </span>
+                                    &nbsp;
+                                    <strong className="text-red-500">
+                                        5<sup>th</sup>&nbsp;November 2024
+                                    </strong>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    &nbsp;Deadline for Camera-Ready Paper
+                                    Submission
+                                </td>
+                                <td>
+                                    <span className="line-through decoration-red-500 text-black">
+                                        &nbsp;
+                                        <strong>
+                                            15<sup>th</sup>&nbsp;October 2024
+                                        </strong>
+                                    </span>
+                                    &nbsp;
+                                    <strong className="text-red-500">
+                                        10<sup>th</sup>&nbsp;November 2024
+                                    </strong>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>&nbsp;Early Registration Deadline</td>
+                                <td>
+                                    <span className="line-through decoration-red-500 text-black">
+                                        &nbsp;
+                                        <strong>
+                                            30<sup>th</sup>&nbsp;September 2024
+                                        </strong>
+                                    </span>
+                                    &nbsp;
+                                    <strong className="text-red-500">
+                                        15<sup>th</sup>&nbsp;October 2024
+                                    </strong>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>&nbsp;Late Registration</td>
+                                <td>
+                                    <span className="text-black">
+                                        &nbsp;
+                                        <strong>
+                                            5<sup>th</sup>&nbsp;November 2024
+                                        </strong>
+                                    </span>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
 
-          <a href="https://cmt3.research.microsoft.com/SUSTAINED2024/" className="text-secondary underline block break-all max-w-full">https://cmt3.research.microsoft.com/SUSTAINED2024/ </a>
-
+                <div className="mt-10">
+                    <h3>Guidelines</h3>
+                    <ol className="flex flex-col gap-2 py-6">
+                        {guidlines.map((item, index) => (
+                            <li key={index} className="list-disc">
+                                {item}
+                            </li>
+                        ))}
+                    </ol>
+                </div>
+                <div>
+                    <h4> IEEE Similarity / Plagiarism Policy:</h4>
+                    <Link
+                        href={
+                            "https://www.ieee.org/publications/rights/plagiarism/plagiarism.html"
+                        }
+                        target="_blank"
+                    >
+                        https://www.ieee.org/publications/rights/plagiarism/plagiarism.html
+                    </Link>
+                </div>
+            </div>
         </div>
-
-        <div className=" flex flex-col gap-5 mt-10">
-          <h3>Important Dates</h3>
-          <table>
-            <tbody>
-              <tr >
-                <td >&nbsp;Deadline for Full Paper Submission</td>
-                <td >
-                  <span className="line-through decoration-red-500 text-black"><strong>&nbsp;30<sup>th</sup>&nbsp;August 2024</strong>&nbsp;</span>
-                  <strong className="text-red-500">&nbsp;30<sup>th</sup>&nbsp;September 2024</strong>&nbsp;
-                </td>
-              </tr>
-              <tr>
-                <td>&nbsp;Notification of Paper Acceptance/Rejection</td>
-                <td>
-                  <span className="line-through decoration-red-500 text-black">&nbsp;<strong>30<sup>th</sup>&nbsp;September 2024</strong></span>
-                  &nbsp;<strong  className="text-red-500">10<sup>th</sup>&nbsp;October 2024</strong>
-                </td>
-              </tr>
-              <tr >
-                <td >&nbsp;Deadline for Camera-Ready Paper Submission</td>
-                <td >
-                  <span className="line-through decoration-red-500 text-black">&nbsp;<strong>15<sup>th</sup>&nbsp;September 2024</strong></span>
-                  &nbsp;<strong className="text-red-500">15<sup>th</sup>&nbsp;October 2024</strong>
-                </td>
-              </tr>
-              <tr >
-                <td >&nbsp;Early Registration Deadline</td>
-                <td >
-                  <span className="line-through decoration-red-500 text-black">&nbsp;<strong>30<sup>th</sup>&nbsp;September 2024</strong></span>
-                  &nbsp;<strong className="text-red-500">15<sup>th</sup>&nbsp;October 2024</strong>
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-
-        <div className="mt-10">
-          <h3>Guidelines</h3>
-          <ol className="flex flex-col gap-2 py-6">{
-            guidlines.map((item, index) => (
-              <li key={index} className="list-disc">{item}</li>
-            ))
-          }
-          </ol>
-        </div>
-        <div>
-          <h4> IEEE Similarity / Plagiarism Policy:</h4>
-          <Link
-            href={'https://www.ieee.org/publications/rights/plagiarism/plagiarism.html'}
-            target="_blank"
-          >
-            https://www.ieee.org/publications/rights/plagiarism/plagiarism.html
-          </Link>
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default CallForPapers;
--- components/HomePage.jsx
+++ components/HomePage.jsx
@@ -8,67 +8,74 @@ import Carousel from "../components/Carousel";
 import { more } from "../data/more";
 import Marquee from "react-fast-marquee";
 const HomePage = ({ homePage, aboutConference, aboutManavRachna }) => {
-  const { title, location, organized, buttonTitle, image } = homePage;
-  const { conferenceTitle, content } = aboutConference;
-  const { collegeTitle, collegeContent } = aboutManavRachna;
-  return (
-    <div className="py-10 px-5 lg:px-10 relative">
-      <div className="container mx-auto">
-        <div className="flex flex-col items-center gap-5 ">
-
-
-          {/* <p className="flex items-center gap-2 text-xl font-bold">
+    const { title, location, organized, buttonTitle, image } = homePage;
+    const { conferenceTitle, content } = aboutConference;
+    const { collegeTitle, collegeContent } = aboutManavRachna;
+    return (
+        <div className="py-10 px-5 lg:px-10 relative">
+            <div className="container mx-auto">
+                <div className="flex flex-col items-center gap-5 ">
+                    {/* <p className="flex items-center gap-2 text-xl font-bold">
             <span>
               <CiCalendarDate />
             </span>
             30<sup>th</sup> - 31<sup>st</sup> August, 2024
           </p> */}
 
-          {/* <p className="flex items-center gap-2 text-xl font-bold">
+                    {/* <p className="flex items-center gap-2 text-xl font-bold">
             <span>
               <FaLocationDot />
             </span>
             {location}
           </p> */}
 
+                    <a
+                        href="https://forms.gle/jXHp1gtJnaxAhUzP7"
+                        target="_blank"
+                        className="absolute left-2 top-1"
+                    >
+                        <Button title="Register Now" />
+                    </a>
+                    <span className="text-[#b22b2f] text-lg font-semibold absolute top-4 md:block hidden">
+                        IEEE CONFERENCE RECORD #63638
+                    </span>
+                    <Marquee className="text-center text-lg font-bold text-secondary">
+                        In response to multiple requests received, we announce
+                        the extension of the deadline for submission of papers
+                        to 31 st October 2024.
+                    </Marquee>
+                    <a
+                        className="absolute right-2 top-1"
+                        href="https://cmt3.research.microsoft.com/User/Login?ReturnUrl=%2FSUSTAINED2024"
+                        target="_black"
+                    >
+                        <Button title="Submission Open" />
+                    </a>
+                    <Carousel />
+                </div>
 
+                <div className="flex flex-col gap-3 items-center justify-center mt-10">
+                    <h3 className="text-center">{conferenceTitle}</h3>
+                    <p className="text-justify">{content}</p>
+                </div>
 
-          <a href="https://forms.gle/jXHp1gtJnaxAhUzP7" target="_blank" className="absolute left-2 top-1">
-            <Button title="Register Now" />
-          </a>
-          <span className="text-[#b22b2f] text-lg font-semibold absolute top-4 md:block hidden">IEEE CONFERENCE RECORD #63638</span>
-          <Marquee className="text-center text-lg font-bold text-secondary">
-            In response to multiple requests received, we announce the extension of the deadline for submission of papers to 30 th September 2024. 
-          </Marquee>
-          <a
-            className="absolute right-2 top-1"
-            href="https://cmt3.research.microsoft.com/User/Login?ReturnUrl=%2FSUSTAINED2024"
-            target="_black"
-          >
-            <Button title="Submission Open" />
-          </a>
-          <Carousel />
+                {more?.map((item, index) => (
+                    <div
+                        className="flex flex-col gap-3 items-center justify-center mt-10"
+                        key={index}
+                    >
+                        <h3 className="text-center">{item.title}</h3>
+                        <p className="text-justify">{item.dec}</p>
+                    </div>
+                ))}
+
+                <div className="flex flex-col gap-3 items-center justify-center mt-10">
+                    <h3 className="text-center">{collegeTitle}</h3>
+                    <p className="text-justify">{collegeContent}</p>
+                </div>
+            </div>
         </div>
-
-        <div className="flex flex-col gap-3 items-center justify-center mt-10">
-          <h3 className="text-center">{conferenceTitle}</h3>
-          <p className="text-justify">{content}</p>
-        </div>
-
-        {more?.map((item, index) => (
-          <div className="flex flex-col gap-3 items-center justify-center mt-10" key={index}>
-            <h3 className="text-center">{item.title}</h3>
-            <p className="text-justify">{item.dec}</p>
-          </div>
-        ))}
-
-        <div className="flex flex-col gap-3 items-center justify-center mt-10">
-          <h3 className="text-center">{collegeTitle}</h3>
-          <p className="text-justify">{collegeContent}</p>
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default HomePage;
